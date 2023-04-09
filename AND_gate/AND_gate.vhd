e8433f222eef5f2b237c6c676991308e11d0be36
ravirajchilka <ravirajchilka10@gmail.com>
2023-04-09 16:42:44 -0400

    Vivado AND gate code

--- AND_gate/AND_gate.vhd
+++ AND_gate/AND_gate.vhd
@@ -1,22 +1,50 @@
-library library IEEE;
-USE IEEE.STD_LOGIC_1164.ALL;
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 01.04.2023 14:16:39
+-- Design Name: 
+-- Module Name: and_gate - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
 
-entity AND_gate is 
-    Port(
-        a: in std_logic;  -- 0,1,z,U,x possible values
-        b: in std_logic;
-        c: out std_logic;
-    );
 
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity AND_gate is  
+ Port ( 
+  a : in std_logic; -- 0, 1, z , U, x
+  b : in std_logic;
+  c : out std_logic
+  
+  );
 end AND_gate;
 
 architecture Behavioral of AND_gate is
-    begin
-        -- Digtal logic goes here
-        c <= a and b;
-        -- c <= '1' when ((a = '1') and (b='1')) else '0';
+begin
+ c <= a and b;
+ 
+ --c <= '1' when ((a = '1') and (b = '1')) else '0';
+ 
 
-end Behavioral;    
-
-
-
+end Behavioral;
--- AND_gate/tb_AND_gate.vhd
+++ AND_gate/tb_AND_gate.vhd
@@ -1,41 +1,75 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 01.04.2023 14:30:15
+-- Design Name: 
+-- Module Name: tb_add_gate - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity tb_AND_gate is
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
 
-end tb_AND_gate;    
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tb_AND_gate is
+--  Port ( );
+end tb_AND_gate;
 
 architecture Behavioral of tb_AND_gate is
-    signal a: std_logic := '0';
-    signal b: std_logic := '0';
-    signal c: std_logic;
+signal a : std_logic := '0';
+signal b : std_logic := '0';
+signal c : std_logic;
 
-component AND_gate is 
-    Port(
-        a: in std_logic; -- 0,1,z,U,x possible values
-        b: in std_logic;
-        c: out std_logic;
-    );
+component and_gate is  
+ Port ( 
+  a : in std_logic; -- 0, 1, z , U, x
+  b : in std_logic;
+  c : out std_logic
+  
+  );
 end component;
-    
+
 begin
-    process begin 
-        a <= '0';
-        b <= '0';
-        wait for 1 ns;
-        b <= '1';
-        wait for 1ns;
-        a <= '1';
-        wait for 1ns;
-        b <= '0';
-        wait for 1ns;
-    end process;   
-    
-i_AND_gate: AND_gate 
-    Port map(
-        a => a;
-        b => b;
-        c => c;
-    );
+
+   process begin
+     a <= '0';
+     b <= '0';
+     wait for 1 ns;
+     b <= '1';
+     wait for 1 ns;
+     a <= '1';
+     wait for 1 ns;
+     b <= '0';
+     wait for 1 ns;
+   end process;
+   
+   i_and_gate : and_gate   
+ Port map ( 
+  a => a,
+  b => b,
+  c => c
+  
+  );
+   
 
 end Behavioral;
