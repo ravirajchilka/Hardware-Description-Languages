6bdf54322c35f4e34550efbda2db01fe104c4c14
Raviraj <ravirajchilka10@gmail.com>
2024-01-23 23:30:00 -0500

    Create 4x2_Priority_Encoder_df.v

---
+++ Verilog_Programs/4x2_Priority_Encoder_df.v
@@ -0,0 +1,13 @@
+module encode_4x2_priority_df(Y,V,I); 
+
+input   [3:0]I; 
+output  [1:0]Y; 
+output  V; 
+
+assign {V,Y} = I[0]?3'b100:I[1]?3'b101:I[2]?3'b110:I[3]?3'b111:3'b000;
+
+
+endmodule
+
+
+
