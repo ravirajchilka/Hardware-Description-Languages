3bb59bab14d2acdea9bd6e816570895bb48658cf
Raviraj <ravirajchilka10@gmail.com>
2024-01-26 23:12:19 -0500

    Create 4bits_comparator_df.v

---
+++ Verilog_Programs/4bits_comparator_df.v
@@ -0,0 +1,13 @@
+module comparator_df(
+    input [3:0]A,B;
+    output Eq,Gt,Sm;
+);
+
+assign Eq = &(A ~^ B); // A==B
+assign Gt = (A[3]7 ~B[3]) | ((A[3]~^B[3]) & (A[2]& ~B[2])) 
+| ((A[3]~^B[3]) & (A[2]~^B[2]) & (A[1]& ~B[1]))
+| ((A[3]~^B[3]) & (A[2]~^B[2]) & (A[1] ~^B[1]) & (A[0]& ~B[0]));
+assign Sm = ~(Gt|Eq);
+
+endmodule
+
