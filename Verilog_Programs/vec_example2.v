482f089f37268c49e9e6ea1d5d2dec0799968ca6
Raviraj <ravirajchilka10@gmail.com>
2024-02-17 22:00:26 -0500

    Update vec_example2.v

--- Verilog_Programs/vec_example2.v
+++ Verilog_Programs/vec_example2.v
@@ -1,9 +1,9 @@
 /*
 A 32-bit vector can be viewed as containing 4 bytes (bits [31:24], [23:16], etc.). 
 Build a circuit that will reverse the byte ordering of the 4-byte word.
+AaaaaaaaBbbbbbbbCcccccccDddddddd => DdddddddCcccccccBbbbbbbbAaaaaaaa
 */
 
-AaaaaaaaBbbbbbbbCcccccccDddddddd => DdddddddCcccccccBbbbbbbbAaaaaaaa
 module top_module( 
     input [31:0] in,
     output [31:0] out );
