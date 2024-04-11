d16cad1562c6242e6d2c9b85d9569d5067736a97
Raviraj <ravirajchilka10@gmail.com>
2024-04-11 00:30:36 -0400

    Update metronome_8_beats_with_7seg_display.v

--- My_Tiny_Projects/Metronome_8 _beats/metronome_8_beats_with_7seg_display.v
+++ My_Tiny_Projects/Metronome_8 _beats/metronome_8_beats_with_7seg_display.v
@@ -3,7 +3,7 @@ module metronome_8_beats_with_7seg_display(
 	output reg a, b, c, d, e, f, g, speaker_out
 );
 
-	reg [3:0] counter_output = 1; // start metronome from beat 1
+	reg [3:0] counter_output = 1'b1; // start metronome from beat 1
 	reg [24:0] counter;
 	reg [16:0] c_sharp_freq_counter;
 	reg [16:0] g_sharp_freq_counter;
@@ -14,50 +14,50 @@ module metronome_8_beats_with_7seg_display(
 	always@(posedge clk) begin 
 		// Generate G# beep at 4th interval // G#4: Around 415.30 Hz 
 		g_sharp_beep_time_counter <= g_sharp_beep_time_counter + 1;
-		if(g_sharp_beep_time_counter == (4*27000000) ) 
-			g_sharp_beep_time_counter <= 0;
+		if(g_sharp_beep_time_counter == (4*25'd27000000) ) 
+			g_sharp_beep_time_counter <= 1'b0;
 
-		if(g_sharp_beep_time_counter >= 3*(27000000) && g_sharp_beep_time_counter < 86400000) begin   // Generate 415.30 Hz frequency
+		if(g_sharp_beep_time_counter >= 3*(25'd27000000) && g_sharp_beep_time_counter < 27'd86400000) begin   // Generate 415.30 Hz frequency
 		    g_sharp_freq_counter <= g_sharp_freq_counter + 1;
-		    if(g_sharp_freq_counter == 65014)  
-			  	g_sharp_freq_counter <= 0;
+		    if(g_sharp_freq_counter == 17'd65014)  
+			  	g_sharp_freq_counter <= 1'b0;
 
-		    if(g_sharp_freq_counter < 65014/2) 
-			  	speaker_out <= 1;
+		    if(g_sharp_freq_counter < 17'd65014/2) 
+			  	speaker_out <= 1'b1;
 		    else 
-			  	speaker_out <= 0;
+			  	speaker_out <= 1'b0;
 		end	
 		
-		c_sharp_loop_counter <= c_sharp_loop_counter + 1;
-		if(c_sharp_loop_counter <= 3*27000000) begin
-			 c_sharp_beep_time_counter <= c_sharp_beep_time_counter + 1;
-			 if(c_sharp_beep_time_counter == 27000000) 
-				 c_sharp_beep_time_counter <= 0;
+		c_sharp_loop_counter <= c_sharp_loop_counter + 1'b1;
+		if(c_sharp_loop_counter <= 3*25'd27000000) begin
+			 c_sharp_beep_time_counter <= c_sharp_beep_time_counter + 1'b1;
+			 if(c_sharp_beep_time_counter == 25'd27000000) 
+				 c_sharp_beep_time_counter <= 1'b0;
 
-			 if(c_sharp_beep_time_counter <= (27000000/5)) begin // Generate C# beep at 1/5th time of a second
-				 c_sharp_freq_counter <= c_sharp_freq_counter + 1;
-				 if(c_sharp_freq_counter == 97408)  
-					 c_sharp_freq_counter <= 0;
-				 if(c_sharp_freq_counter < 97408/2) 
-					 speaker_out <= 1;
+			 if(c_sharp_beep_time_counter <= (25'd27000000/5)) begin // Generate C# beep at 1/5th time of a second
+				 c_sharp_freq_counter <= c_sharp_freq_counter + 1'b1;
+				 if(c_sharp_freq_counter == 17'd97408)  
+					 c_sharp_freq_counter <= 1'b0;
+				 if(c_sharp_freq_counter < 17'd97408/2) 
+					 speaker_out <= 1'b1;
 				 else 
-					 speaker_out <= 0;
+					 speaker_out <= 1'b0;
 			 end	
 		end
-		else if(c_sharp_loop_counter == 4*27000000) begin 
-			c_sharp_loop_counter <= 0;
+		else if(c_sharp_loop_counter == 4*25'd27000000) begin 
+			c_sharp_loop_counter <= 1'b0;
 		end
 						
 		// 7-segment display 
-		if(counter == 27000000) begin 
-			counter <= 0;
-			counter_output <= counter_output + 1;
+		if(counter == 25'd27000000) begin 
+			counter <= 1'b0;
+			counter_output <= counter_output + 1'b1;
 			if(counter_output == 4'b1000) begin 
-				counter_output <= 1; // start metronome from beat 1
+				counter_output <= 1'b1; // start metronome from beat 1
 			end
 		end
 		else begin 
-			counter <= counter + 1;
+			counter <= counter + 1'b1;
 		end
 	end
 
